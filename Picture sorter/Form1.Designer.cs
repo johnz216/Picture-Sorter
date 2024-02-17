bc26b5cc41ce9120ced3801027793e1ac8934143
johnz216 <shroomhead3@yahoo.com>
2024-02-17 10:06:02 -0500

    initial upload
    initial upload

---
+++ Picture sorter/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace Picture_sorter
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new System.Windows.Forms.Button();
+            label1 = new System.Windows.Forms.Label();
+            lblWork = new System.Windows.Forms.Label();
+            txtFrom = new System.Windows.Forms.TextBox();
+            label2 = new System.Windows.Forms.Label();
+            label3 = new System.Windows.Forms.Label();
+            txtTo = new System.Windows.Forms.TextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new System.Drawing.Point(110, 198);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(75, 32);
+            button1.TabIndex = 0;
+            button1.Text = "DO IT";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(12, 233);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(131, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Currently working on... ";
+            // 
+            // lblWork
+            // 
+            lblWork.AutoSize = true;
+            lblWork.Location = new System.Drawing.Point(13, 157);
+            lblWork.Name = "lblWork";
+            lblWork.Size = new System.Drawing.Size(0, 15);
+            lblWork.TabIndex = 2;
+            // 
+            // txtFrom
+            // 
+            txtFrom.Location = new System.Drawing.Point(25, 53);
+            txtFrom.Name = "txtFrom";
+            txtFrom.Size = new System.Drawing.Size(284, 23);
+            txtFrom.TabIndex = 3;
+            txtFrom.Text = "M:\\JoNo\\Pics\\to sort";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(25, 32);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(133, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Start from this Directory";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new System.Drawing.Point(25, 93);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(114, 15);
+            label3.TabIndex = 5;
+            label3.Text = "Sort to this directory";
+            // 
+            // txtTo
+            // 
+            txtTo.Location = new System.Drawing.Point(25, 111);
+            txtTo.Name = "txtTo";
+            txtTo.Size = new System.Drawing.Size(284, 23);
+            txtTo.TabIndex = 6;
+            txtTo.Text = "M:\\JoNo\\Pics\\Sorted";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(321, 417);
+            Controls.Add(txtTo);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(txtFrom);
+            Controls.Add(lblWork);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblWork;
+        private System.Windows.Forms.TextBox txtFrom;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTo;
+    }
+}
+
---
+++ Picture sorter/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ExifLib;
+
+namespace Picture_sorter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var eo = new System.IO.EnumerationOptions();
+            eo.RecurseSubdirectories = true;
+
+            foreach  (var dir in System.IO.Directory.GetDirectories(txtFrom.Text, "*", eo))
+            {
+                foreach (var file in System.IO.Directory.GetFiles(dir))
+                {
+                    SetWorking(file);
+                    var ispic = isPic(file);
+                    var dateTaken = new DateTime();
+                    if (ispic)
+                    {
+                        dateTaken = getFileCreationDate(file);
+                        saveCopy(file, dateTaken);
+                    }
+                    else
+                    {
+                        saveCopyNonPic(file);
+                    }                     
+                }
+            }
+
+            MessageBox.Show("DONE!");
+        }
+
+        private bool isPic(string file)
+        {
+            if (file.ToUpper().EndsWith("JPG"))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void saveCopyNonPic(string file)
+        {
+            var rnd = new Random();
+
+            var fileName = file.Substring(file.LastIndexOf(@"\") + 1);
+            var destionationFolder = (txtTo.Text.EndsWith(@"\")) ? txtTo.Text + @"NotPictures\" : txtTo.Text + @"\NotPictures\";
+
+            if (!System.IO.Directory.Exists(destionationFolder))
+                System.IO.Directory.CreateDirectory(destionationFolder);
+
+            var newFileName = String.Concat(destionationFolder, fileName);
+
+            //if (System.IO.File.Exists(newFileName))
+            //    newFileName = string.Concat(newFileName.Substring(0, newFileName.LastIndexOf(".") - 1), "_", rnd.Next(10, 9999), newFileName.Substring(newFileName.LastIndexOf(".")));
+            //System.IO.File.Copy(file, newFileName);
+
+            if (System.IO.File.Exists(newFileName))
+            {
+                var newFileCreationTime = System.IO.File.GetCreationTimeUtc(newFileName);
+                var oldFileCreationTime = System.IO.File.GetCreationTimeUtc(file);
+
+                if (newFileCreationTime > oldFileCreationTime)
+                {
+                    System.IO.File.Copy(file, newFileName, true);
+                }
+            }
+            else
+            {
+                System.IO.File.Copy(file, newFileName);
+            }
+        }
+
+        private void saveCopy(string file, DateTime dateTaken)
+        {
+            var rnd = new Random();
+            var fileExt = file.Substring(file.LastIndexOf("."));
+            
+            var toFolder = (txtTo.Text.EndsWith(@"\")) ? txtTo.Text : txtTo.Text + @"\";
+            var destionationFolder = toFolder + dateTaken.Year + @"\" + dateTaken.Month + @"\";
+            
+            if (!System.IO.Directory.Exists(destionationFolder))
+                System.IO.Directory.CreateDirectory(destionationFolder);
+
+            var newFileName = destionationFolder + String.Concat(dateTaken.ToString("yyyyMMddHHmmss"), "_", rnd.Next(10, 9999), fileExt);
+
+            if (System.IO.File.Exists(newFileName))
+            {
+                var newPicCreationTime = System.IO.File.GetCreationTimeUtc(newFileName);
+                var oldPicCreationTime = System.IO.File.GetCreationTimeUtc(file);
+
+                if (newPicCreationTime > oldPicCreationTime)
+                {
+                    System.IO.File.Copy(file, newFileName, true);
+                }
+            }
+            else
+            {
+                System.IO.File.Copy(file, newFileName);
+            }
+        }
+
+        private void SetWorking(string working)
+        {
+            lblWork.Text = working;
+            lblWork.Invalidate();
+            lblWork.Update();
+            lblWork.Refresh();
+            Application.DoEvents();
+        }
+
+        private DateTime getFileCreationDate (string file)
+        {
+            DateTime datePictureTaken;
+            try
+            {
+                using (ExifReader reader = new ExifReader(file))
+                {
+                    // Extract the tag data using the ExifTags enumeration                
+                    if (reader.GetTagValue<DateTime>(ExifTags.DateTimeDigitized, out datePictureTaken))
+                    {
+                        return datePictureTaken;
+                    }
+                }
+            }
+            catch
+            {
+                datePictureTaken = new DateTime(1900, 01, 01, 1, 0, 0);
+            }
+            
+            
+            return datePictureTaken;
+        }
+    }
+}
